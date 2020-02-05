eaf2bd1f2dffddfa87a26ce880817d4e5aaa531f
Matt Dunson <dunson.142000@gmail.com>
2020-02-05 10:49:44 -0500

    added profile page and link to profile page for authenticated users to demo authentication

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React from "react";
 import NavBar from "./components/NavBar";
-import { useAuth0 } from "./react-auth0-spa";
+
+import { Router, Route, Switch } from "react-router-dom";
+import Profile from "./components/Profile";
+import history from "./utils/history";
 
 function App() {
-  const { loading } = useAuth0();
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
   return (
     <div className="App">
-      <header>
-        <NavBar />
-      </header>
+      {/* Don't forget to include the history module */}
+      <Router history={history}>
+        <header>
+          <NavBar />
+        </header>
+        <Switch>
+          <Route path="/" exact />
+          <Route path="/profile" component={Profile} />
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { useAuth0 } from "../react-auth0-spa";
 
+import { Link } from "react-router-dom";
+
 const NavBar = () => {
     const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
 
@@ -11,6 +13,12 @@ const NavBar = () => {
             )}
 
             {isAuthenticated && <button onClick={() => logout()}>Log out</button>}
+            {isAuthenticated && (
+                <span>
+                    <Link to="/">Home</Link>&nbsp;
+                    <Link to="/profile">Profile</Link>
+                </span>
+            )}
         </div>
     );
 };
