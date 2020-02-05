1e44be78371621c4c2ae6732e63c81eb36e79f0a
Matt Dunson <dunson.142000@gmail.com>
2020-02-05 10:03:43 -0500

    Created basic Auth0 app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import NavBar from "./components/NavBar";
+import { useAuth0 } from "./react-auth0-spa";
 
 function App() {
+  const { loading } = useAuth0();
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <NavBar />
       </header>
     </div>
   );
---
+++ src/components/NavBar.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { useAuth0 } from "../react-auth0-spa";
+
+const NavBar = () => {
+    const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
+
+    return (
+        <div>
+            {!isAuthenticated && (
+                <button onClick={() => loginWithRedirect({})}>Log in</button>
+            )}
+
+            {isAuthenticated && <button onClick={() => logout()}>Log out</button>}
+        </div>
+    );
+};
+
+export default NavBar;
